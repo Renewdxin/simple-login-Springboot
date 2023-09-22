838eaa9328334f49db03a44809e234b51cfa4570
Renewdxin <cissyrenxin@icloud.com>
2023-09-22 20:04:46 +0800

    the first

--- back/src/main/java/com/example/config/SecurityConfiguration.java
+++ back/src/main/java/com/example/config/SecurityConfiguration.java
@@ -1,12 +1,19 @@
 package com.example.config;
 
+import com.alibaba.fastjson.JSONObject;
+import com.example.entity.RestBean;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -16,17 +23,31 @@ public class SecurityConfiguration {
         return security
                 .authorizeHttpRequests((authorizeRequests) ->
                         authorizeRequests
-                                .requestMatchers("/**"))
+                                .anyRequest().authenticated())
                 .formLogin((formlogin) ->
                         formlogin
                                 .loginProcessingUrl("/api/auth/login")
+                                .successHandler(this::onAuthenticationSuccess)
+                                .failureHandler(this::onAuthenticationFailure)
                 )
                 .logout((logout) ->
                         logout
                                 .logoutUrl("/api/auth/logout"))
                 .csrf(AbstractHttpConfigurer::disable)
+                .exceptionHandling((handling) ->
+                        handling
+                                .authenticationEntryPoint(this::onAuthenticationFailure))
+
                 .build();
+    }
 
+    private void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException e) throws IOException {
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write(JSONObject.toJSONString(RestBean.failure(401, e.getMessage())));
+    }
 
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write(JSONObject.toJSONString(RestBean.success("SUCCESS")));
     }
 }
