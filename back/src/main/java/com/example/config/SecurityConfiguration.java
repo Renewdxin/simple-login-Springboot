08faa932545f93c5d4696726cb167842cbc360dd
Renewdxin <cissyrenxin@icloud.com>
2023-09-23 12:39:47 +0800

    2023.9.23

--- back/src/main/java/com/example/config/SecurityConfiguration.java
+++ back/src/main/java/com/example/config/SecurityConfiguration.java
@@ -2,22 +2,31 @@ package com.example.config;
 
 import com.alibaba.fastjson.JSONObject;
 import com.example.entity.RestBean;
+import com.example.service.authorizeService;
+import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import java.io.IOException;
 
+@SuppressWarnings("ALL")
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+    @Resource
+    authorizeService service;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity security) throws Exception {
         return security
@@ -33,12 +42,27 @@ public class SecurityConfiguration {
                 .logout((logout) ->
                         logout
                                 .logoutUrl("/api/auth/logout"))
+                //.userDetailsService(service)
                 .csrf(AbstractHttpConfigurer::disable)
                 .exceptionHandling((handling) ->
                         handling
                                 .authenticationEntryPoint(this::onAuthenticationFailure))
 
                 .build();
+    }
+
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity security) throws Exception {
+        return security
+                .getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(service)
+                .and().build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     private void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException e) throws IOException {
