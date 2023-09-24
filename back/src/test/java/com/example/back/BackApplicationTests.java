8e289e1871d81e98090e5f4d7040f2d262b9f76c
Renewdxin <cissyrenxin@icloud.com>
2023-09-24 21:42:48 +0800

    9.24

--- back/src/test/java/com/example/back/BackApplicationTests.java
+++ back/src/test/java/com/example/back/BackApplicationTests.java
@@ -2,8 +2,6 @@ package com.example.back;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootTest
 class BackApplicationTests {
